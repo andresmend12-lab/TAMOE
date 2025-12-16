25f5739d416c33307d9188d5955fc44149796656
andresmend12 <andresmend12@gmail.com>
2025-12-16 18:56:19 +0000

    españolizando

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,11 +1,9 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggleButton = document.getElementById('theme-toggle');
 
     if (themeToggleButton) {
-        const themeIcon = themeToggleButton.querySelector('span'); // Get the icon element
+        const themeIcon = themeToggleButton.querySelector('span');
 
-        // Function to update the icon based on the current theme
         const updateIcon = () => {
             if (document.documentElement.classList.contains('dark')) {
                 themeIcon.textContent = 'light_mode';
@@ -14,27 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         };
 
-        // On page load, set the correct theme and icon
-        if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        // Set initial theme based on localStorage
+        if (localStorage.getItem('theme') === 'dark') {
             document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
         }
-        updateIcon(); // Set the initial icon
+        updateIcon();
 
-        // Add click event listener
         themeToggleButton.addEventListener('click', () => {
-            // Toggle the 'dark' class on the root <html> element
             document.documentElement.classList.toggle('dark');
 
-            // Save the theme preference to localStorage
+            let theme = 'light';
             if (document.documentElement.classList.contains('dark')) {
-                localStorage.setItem('theme', 'dark');
-            } else {
-                localStorage.setItem('theme', 'light');
+                theme = 'dark';
             }
+            localStorage.setItem('theme', theme);
 
-            // Update the icon
             updateIcon();
         });
     }
