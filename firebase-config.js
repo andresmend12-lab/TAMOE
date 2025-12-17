42851a029e4a1bc0fbe349a963690840a5de0a98
andresmend12-lab <andresmend12@gmail.com>
2025-12-17 09:00:36 +0100

    mas cambios

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@
   authDomain: "tamoe-86320208-a33cf.firebaseapp.com",
   databaseURL: "https://tamoe-86320208-a33cf-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "tamoe-86320208-a33cf",
-  storageBucket: "tamoe-86320208-a33cf.firebasestorage.app",
+  storageBucket: "tamoe-86320208-a33cf.appspot.com",
   messagingSenderId: "322898199328",
   appId: "1:322898199328:web:89c76af9513534ebf40d45"
 };
--- firebase.js
+++ firebase.js
@@ -1,22 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+﻿import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDidzpojgcd8wMtFLXyXRmZMk6BHELEbSI",
   authDomain: "tamoe-86320208-a33cf.firebaseapp.com",
   databaseURL: "https://tamoe-86320208-a33cf-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "tamoe-86320208-a33cf",
-  storageBucket: "tamoe-86320208-a33cf.firebasestorage.app",
+  storageBucket: "tamoe-86320208-a33cf.appspot.com",
   messagingSenderId: "322898199328",
   appId: "1:322898199328:web:89c76af9513534ebf40d45"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Export auth and database services
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const database = getDatabase(app);
+export const storage = getStorage(app);
+export const firestore = getFirestore(app);
